78040083529f4a6f267c925ca5948df04ee4c2d0
aidanazkafaro <aidan.azkafaro@ui.ac.id>
2022-06-04 13:54:09 +0700

    bikin accordiom dan buat calendar

--- src/pages/PageMisi.js
+++ src/pages/PageMisi.js
@@ -1,22 +1,29 @@
-import React, { useState } from 'react'
-import Footer from '../components/footer/Footer';
-import Navbar from '../components/Header/Navbar';
-import Misi from '../components/Misi/Misi';
-import Sponsor from '../components/Sponsor/Sponsor';
+import React, { useState } from "react";
+import Footer from "../components/footer/Footer";
+import Navbar from "../components/Header/Navbar";
+import Misi from "../components/Misi/Misi";
+import Sponsor from "../components/Sponsor/Sponsor";
+import { misiItems } from "../components/Misi/MisiData";
 
 const PageMisi = () => {
-    const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
-    const toggle = () => {
-      setIsOpen(!isOpen);
-    };
-    return (
-      <>
-        <Navbar isOpen={isOpen} setIsOpen={toggle} />
-        <Misi />
-        <Sponsor />
-        <Footer />
-      </>
-    );
+  const toggle = () => {
+    setIsOpen(!isOpen);
   };
-export default PageMisi
+
+
+  return (
+    <>
+      <Navbar isOpen={isOpen} setIsOpen={toggle} />
+      <div className="p-10 md:px-32 md:pt-20">
+      {misiItems.map((misiItem, id) => (
+          <Misi misiItem={misiItem}/>
+        ))}
+      </div>
+      <Sponsor />
+      <Footer />
+    </>
+  );
+};
+export default PageMisi;
