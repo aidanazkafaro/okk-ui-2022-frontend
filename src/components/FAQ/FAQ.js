ef9ff1147e6eb2ed1596869065343d924b2e6625
risalstr <93498117+risalstrr@users.noreply.github.com>
2022-06-20 16:50:26 +0700

    refactor code

--- src/components/FAQ/FAQ.js
+++ src/components/FAQ/FAQ.js
@@ -4,23 +4,30 @@ import { FAQData } from "./FAQData";
 const FAQ = () => {
   return (
     <>
-      <div className='mx-36 mt-5 mb-60'>
-        <h1 className='underline decoration-[#E1AA43] mb-5 text-4xl'>Frequently Asked Questions</h1>
-        <div className='bg-[#F9E1CF] rounded-xl'>
+      <div className='xl:mx-36 md:mx-20 mx-12 mt-20 mb-60'>
+        <div className="flex items-right justify-right">
+          <h1 className="font-signika xl:text-5xl md:text-4xl text-3xl font-semibold">Frequently Asked Questions</h1>
+        </div>
+        <div className="flex items-right justify-right">
+          <div
+            className="container xl:w-[600px] md:w-[450px] w-[380px] h-2 mt-1 bg-underlineOrange rounded-xl mb-8"
+          />
+        </div>
+        <div className='rounded-xl'>
           {FAQData.map((FAQ, id) => (
-            <div key={id} className="relative bg-[#F9E1CF] overflow-hidden rounded-xl p-5">
+            <div key={id} className="relative mt-4 bg-underlineOrange overflow-hidden rounded-xl p-2">
               <input type="checkbox" className="peer absolute top-0 inset-x-0 w-full h-12 opacity-0 z-10 cursor-pointer" />
-              <div className="h-12 w-full pl-5 flex items-center">
-                <h1>
+              <div className="h-12 w-full pl-5 flex items-center max-h-full">
+                <h1 className='xl:text-xl font-bold md:text-lg text-sm mr-5'>
                   {FAQ.title}
                 </h1>
               </div>
-              <div className="absolute top-3 right-3 transition-transform duration-500 rotate-0 peer-checked:-rotate-90">
+              <div className="absolute top-5 right-3 transition-transform duration-500 rotate-0 peer-checked:-rotate-90">
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 19l-7-7 7-7" />
                 </svg>
               </div>
-              <div className="overflow-hidden bg-[#F9E1CF] transition-all duration-500 max-h-0 peer-checked:max-h-40">
+              <div className="overflow-hidden rounded-lg bg-[#F9E1CF] transition-all duration-500 max-h-0 peer-checked:max-h-full">
                 <div className="p-5 border-t">{FAQ.question}</div>
               </div>
             </div>
--- src/components/FAQ/FAQData.js
+++ src/components/FAQ/FAQData.js
@@ -1,13 +1,19 @@
 export const FAQData = [{
-    title: "Apa maksudnya ini?",
+    title: "Consectetur adipiscing elit?",
     question: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Facilisi etiam dignissim diam quis enim lobortis",
 },
 {
-    title: "Apa maksudnya ini lorem ipsum?",
+    title: "Lorem ipsum dolor sit amet?",
     question: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Facilisi etiam dignissim diam quis enim lobortis",
 },
 {
-    title: "Apa maksudnya consectetur adipiscing elit?",
+    title: "How long?",
     question: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Facilisi etiam dignissim diam quis enim lobortis",
 },
+{
+    title: "Veritatis repellendus ea sequi pariatur?",
+    question: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis expedita voluptatem aspernatur suscipit dignissimos fuga est dolore tempora excepturi? Quis cupiditate veritatis repellendus ea sequi pariatur, alias dolore perspiciatis at in consequuntur tempora voluptatum nemo tenetur harum molestiae dolorem. Hic tempore recusandae aliquid, laudantium, quidem voluptatem amet voluptates similique velit possimus quo non? Quae dignissimos beatae libero aliquam sit nam at facilis magnam ut repellendus ipsam eveniet illum dicta harum rerum ea numquam praesentium aspernatur deleniti quia nihil officia animi, non qui? Earum inventore delectus vel id quibusdam? Iure, quia maxime. Libero ut beatae harum commodi porro incidunt accusantium numquam! Blanditiis expedita voluptatem aspernatur suscipit dignissimos fuga est dolore tempora excepturi? Quis cupiditate veritatis repellendus ea sequi pariatur, alias dolore perspiciatis at in consequuntur tempora voluptatum nemo tenetur harum molestiae dolorem. Hic tempore recusandae aliquid, laudantium, quidem voluptatem amet voluptates similique velit possimus quo non? Quae dignissimos beatae libero aliquam sit nam at facilis magnam ut repellendus ipsam eveniet illum dicta harum rerum ea numquam praesentium aspernatur deleniti quia nihil officia animi, non qui? Earum inventore delectus vel id quibusdam? Iure, quia maxime. Libero ut beatae harum commodi porro incidunt accusantium numquam!",
+},
+
 ];
+
