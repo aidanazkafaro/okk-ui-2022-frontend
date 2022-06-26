d1d963e0f83ec19076a0846f8effc8e05a328bd1
embunazzahra <dhauembunazzahra@gmail.com>
2022-06-26 21:54:28 +0700

    more responsive

--- src/components/CariKelompok/HeaderCariKelompok.js
+++ src/components/CariKelompok/HeaderCariKelompok.js
@@ -3,6 +3,7 @@ import "./HeaderCariKelompok.css";
 import loop from "../../assets/crop-loop.svg";
 import book from "../../assets/Written book.svg";
 import garland from "../../assets/Garland.svg";
+import hasil from "../../assets/hasil.png";
 import CariKelompok from "./CariKelompok";
 
 const HeaderCariKelompok = () => {
@@ -10,17 +11,17 @@ const HeaderCariKelompok = () => {
     <div className="w-full h-full">
       {/* header */}
       <section className="relative bg-[#F9E1CF] items-center flex -top-20 h-full">
-        <div className="bg-header-carikelompok bg-cover w-full pt-36 header-bg-section sm:pt-36">
+        <div className="bg-header-carikelompok bg-cover w-full pt-36 header-bg-section sm:pt-60">
           <div className="flex items-center justify-center flex-col h-full">
             <h1 className="h1-title text-4xl mb-2 tracking-wide text-white font-bold xl:text-7xl lg:text-6xl lg:mb-8 md:text-5xl md:mb-7 sm:text-4xl sm:mb-7">
               Cari Kelompok
             </h1>
             <div className="relative flex items-center justify-center w-full">
-              <img src={loop} alt="LOOP" className="w-1/2 sm:w-1/4" />
+              <img src={loop} alt="LOOP" className="w-1/2 md:w-1/4" />
               <img
                 src={book}
                 alt="BOOK"
-                className="absolute z-10 top-[47%] w-2/3 sm:w-2/5"
+                className="absolute z-10 top-[47%] w-2/3 md:w-2/5"
               />
             </div>
           </div>
@@ -30,15 +31,17 @@ const HeaderCariKelompok = () => {
         <img className="absolute garland" alt="" src={garland} />
       </div>
       {/* body */}
-      <div className="carikelompok-body flex items-center max-w-[1400px] mx-auto px-5 sm:px-10 md:px-10 lg:px-24 2xl:px-32 3xl:px-0">
+      <div className="flex justify-evenly carikelompok-body px-5 sm:px-10 md:px-8 lg:px-24 2xl:px-32 3xl:px-0">
         {/* form-side */}
-        <div className="p-0 md:w-1/2 mr-8">
-          <div className="rounded-xl bg-[#364130] pt-28 pb-20 pl-10 flex items-center justify-center">
+        <div className="p-0 md:w-[47%] lg:w-2/5 flex items-center justify-center">
+          <div className="rounded-3xl bg-[#364130] w-full aspect-square px-10 flex items-center justify-center">
             <CariKelompok />
           </div>
         </div>
         {/* result-side */}
-        <div className="p-0 md:w-1/2"></div>
+        <div className="p-0 my-10 md:my-0 md:w-[47%] lg:w-2/5 flex items-center justify-center">
+          <img alt="hasil" src={hasil} />
+        </div>
       </div>
     </div>
   );
