3b66a34c2e5fd1adf518472bbfa2f2383ae1d315
Ariq Pradipa Santoso <ariq.pradipa@outlook.com>
2022-07-20 19:56:12 +0700

    nambahin semua merch

--- src/components/Merch/Merch.js
+++ src/components/Merch/Merch.js
@@ -16,7 +16,7 @@ import tumbler from '../../assets/merch/tumbler/tumbler.png';
 export default function ActionAreaCard() {
     return (
         <>
-            <div className="flex items-right justify-center pt-[2%]">
+            <div className="flex items-right justify-center pt-[4%]">
                 <h1 className="font-signika xl:text-5xl md:text-4xl text-3xl font-semibold">
                     OKK UI MERCHANDISE
                 </h1>
@@ -29,7 +29,7 @@ export default function ActionAreaCard() {
                     <div class="flex flex-wrap -m-1 md:-m-2">
                         <div class="flex flex-wrap w-1/2">
                             <div class="w-1/2 p-1 md:p-2">
-                                <Card sx={{ maxWidth: 500 }}>
+                                <Card sx={{ maxWidth: 400 }}>
                                     <CardActionArea>
                                         <CardMedia
                                             component="img"
@@ -41,78 +41,62 @@ export default function ActionAreaCard() {
                                             <Typography gutterBottom variant="h5" component="div">
                                                 Totebag
                                             </Typography>
-                                            <Typography variant="body2" color="text.secondary">
-
-                                            </Typography>
+                                            <div className="hidden md:flex">
+                                                <Typography variant="body2" color="text.secondary">
+                                                    Bawaanmu banyak? bingung mau bawanya gimana? tenang! dengan totebag OKK UI akan memudahkan kamu dalam membawa barang barang.
+                                                </Typography>
+                                            </div>
                                         </CardContent>
                                     </CardActionArea>
                                 </Card>
                             </div>
-                            <div class="w-1/2 p-1 md:p-2">
-                                <Card sx={{ maxWidth: 400 }}>
+                            <div class="w-1/2 p-0 md:p-2 pt-1">
+                                <Card sx={{ maxWidth: 350 }}>
                                     <CardActionArea>
                                         <CardMedia
                                             component="img"
                                             height="140"
-                                            image={stiker}
-                                            alt="Stiker"
-                                        />
-                                        <CardContent>
-                                            <Typography gutterBottom variant="h5" component="div">
-                                                Stiker
-                                            </Typography>
-                                            <Typography variant="body2" color="text.secondary">
-                                                {/*fill with description */}
-                                            </Typography>
-                                        </CardContent>
-                                    </CardActionArea>
-                                </Card>
-
-                            </div>
-                            <div class="w-full p-1 md:p-2">
-                                <Card sx={{ maxWidth: 800 }}>
-                                    <CardActionArea>
-                                        <CardMedia
-                                            component="img"
-                                            height="140"
-                                            image={notebook}
+                                            image={gelang}
                                             alt="totebag"
                                         />
                                         <CardContent>
                                             <Typography gutterBottom variant="h5" component="div">
-                                                Notebook
+                                                Gelang
                                             </Typography>
-                                            <Typography variant="body2" color="text.secondary">
-                                                Bingung dengan catatan kuliah? Cobalah mencatat menggunakan notebook OKK UI ini! Buku ini dipercaya dapat memudahkan dalam belajar.
-                                            </Typography>
-                                        </CardContent>
-                                    </CardActionArea>
-                                </Card>
-                            </div>
-                        </div>
-                        <div class="flex flex-wrap w-1/2">
-                            <div class="w-full p-1 md:p-2">
-                                <Card sx={{ maxWidth: 650 }}>
-                                    <CardActionArea>
-                                        <CardMedia
-                                            component="img"
-                                            height="140"
-                                            image={baju}
-                                            alt="baju"
-                                        />
-                                        <CardContent>
-                                            <Typography gutterBottom variant="h5" component="div">
-                                                Baju OKK UI
-                                            </Typography>
-                                            <Typography variant="body2" color="text.secondary">
-                                                Kamu anak UI tapi belum punya baju OKK UI?!?! NANI!? Segera dapatkan baju OKK UI dan rasakan benefitnya!
-                                            </Typography>
+                                            <div className="hidden md:flex">
+                                                <Typography variant="body2" color="text.secondary">
+                                                    Tangan kerasa kosong? ingin menghiasi tangan kamu dengan sesuatu yang keren? pakailah gelang OKK UI!
+                                                </Typography>
+                                            </div>
                                         </CardContent>
                                     </CardActionArea>
                                 </Card>
                             </div>
                             <div class="w-1/2 p-1 md:p-2">
-                                <Card sx={{ maxWidth: 400 }}>
+                                <Card sx={{ maxWidth: 350 }}>
+                                    <CardActionArea>
+                                        <CardMedia
+                                            component="img"
+                                            height="140"
+                                            image={emblem}
+                                            alt="Stiker"
+                                        />
+                                        <CardContent>
+                                            <Typography gutterBottom variant="h5" component="div">
+                                                Emblem
+                                            </Typography>
+                                            <div className="hidden md:flex">
+                                                <Typography variant="body2" color="text.secondary">
+                                                    Sebuah emblem yang keren abis.
+                                                </Typography>
+                                            </div>
+                                        </CardContent>
+                                    </CardActionArea>
+                                </Card>
+
+                            </div>
+                            <div class="w-1/2 p-1 md:p-2">
+                                <Card sx={{ maxWidth: 350 }}>
                                     <CardActionArea>
                                         <CardMedia
                                             component="img"
@@ -124,15 +108,88 @@ export default function ActionAreaCard() {
                                             <Typography gutterBottom variant="h5" component="div">
                                                 Topi
                                             </Typography>
-                                            <Typography variant="body2" color="text.secondary">
-                                                Depok panas?! Tenang saja! dengan topi OKK UI ini kamu akan terlindung dari sinar matahari yang terik.
+                                            <div className="hidden md:flex">
+                                                <Typography variant="body2" color="text.secondary">
+                                                    Depok panas?! Tenang saja! dengan topi OKK UI ini kamu akan terlindung dari sinar matahari yang terik.
+                                                </Typography>
+                                            </div>
+                                        </CardContent>
+                                    </CardActionArea>
+                                </Card>
+
+                            </div>
+                            
+                            <div class="w-full p-1 md:p-2">
+                                <Card sx={{ maxWidth: 700 }}>
+                                    <CardActionArea>
+                                        <CardMedia
+                                            component="img"
+                                            height="140"
+                                            image={notebook}
+                                            alt="totebag"
+                                        />
+                                        <CardContent>
+                                            <Typography gutterBottom variant="h5" component="div">
+                                                Notebook
                                             </Typography>
+                                            <div className="hidden md:flex">
+                                                <Typography variant="body2" color="text.secondary">
+                                                    Bingung dengan catatan kuliah? Cobalah mencatat menggunakan notebook OKK UI ini! Buku ini dipercaya dapat memudahkan dalam belajar.
+                                                </Typography>
+                                            </div>
                                         </CardContent>
                                     </CardActionArea>
                                 </Card>
                             </div>
+                        </div>
+                        <div class="flex flex-wrap w-1/2 pb-[20%]">
+                            <div class="w-full p-1 md:p-2">
+                                <Card sx={{ maxWidth: 600 }}>
+                                    <CardActionArea>
+                                        <CardMedia
+                                            component="img"
+                                            height="140"
+                                            image={baju}
+                                            alt="baju"
+                                        />
+                                        <CardContent>
+                                            <Typography gutterBottom variant="h5" component="div">
+                                                Baju OKK UI
+                                            </Typography>
+                                            <div className="hidden md:flex">
+                                                <Typography variant="body2" color="text.secondary">
+                                                    Kamu anak UI tapi belum punya baju OKK UI?!?! NANI!? Segera dapatkan baju OKK UI dan rasakan benefitnya!
+                                                </Typography>
+                                            </div>
+                                        </CardContent>
+                                    </CardActionArea>
+                                </Card>
+                            </div>
+                            <div class="w-1/2 p-1 md:p-2">
+                                <Card sx={{ maxWidth: 350 }}>
+                                    <CardActionArea>
+                                        <CardMedia
+                                            component="img"
+                                            height="140"
+                                            image={stiker}
+                                            alt="Stiker"
+                                        />
+                                        <CardContent>
+                                            <Typography gutterBottom variant="h5" component="div">
+                                                Stiker
+                                            </Typography>
+                                            <div className="hidden md:flex">
+                                                <Typography variant="body2" color="text.secondary">
+                                                    Bingung cara menunjukkan identitasmu? tenang! dengan stiker OKK UI ini kamu dapat menunjukkan identitas kamu ke orang-orang!
+                                                </Typography>
+                                            </div>
+                                        </CardContent>
+                                    </CardActionArea>
+                                </Card>
+
+                            </div>
                             <div class="w-1/2 p-0 md:p-2">
-                                <Card sx={{ maxWidth: 270 }}>
+                                <Card sx={{ maxWidth: 220 }}>
                                     <CardActionArea>
                                         <CardMedia
                                             component="img"
@@ -144,14 +201,17 @@ export default function ActionAreaCard() {
                                             <Typography gutterBottom variant="h5" component="div">
                                                 Tumblr
                                             </Typography>
-                                            <Typography variant="body2" color="text.secondary">
-                                                Takut kehausan saat jalan-jalan di UI? sekarang kamu bisa menyimpan minuman menggunakan
-                                                tumbler OKKUI! sehingga kamu dapat minum kapanpun dan dimanapun.
-                                            </Typography>
+                                            <div className="hidden md:flex">
+                                                <Typography variant="body2" color="text.secondary">
+                                                    Takut kehausan saat jalan-jalan di UI? sekarang kamu bisa menyimpan minuman menggunakan
+                                                    tumbler OKKUI! sehingga kamu dapat minum kapanpun dan dimanapun.
+                                                </Typography>
+                                            </div>
                                         </CardContent>
                                     </CardActionArea>
                                 </Card>
                             </div>
+
                         </div>
                     </div>
                 </div>
