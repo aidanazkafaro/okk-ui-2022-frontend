202f0300182d5478955c64ca30cd6cba97052c06
Ariq Pradipa Santoso <ariq.pradipa@outlook.com>
2022-07-20 18:27:16 +0700

    add initial merch

---
+++ src/pages/PageMerch.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import Merch from "../components/Merch/Merch";
+import Footer from "../components/footer/Footer";
+import Navbar from "../components/Header/Navbar";
+import Sponsor from "../components/Sponsor/Sponsor";
+
+const PageMerch = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => {
+    setIsOpen(!isOpen);
+  };
+  return (
+    <div className={`${isOpen ? "fixed h-screen w-full" : ""} flex flex-col min-h-screen min-w-[300px]`}>
+      <Navbar isOpen={isOpen} setIsOpen={toggle} />
+      
+      <Merch />
+      {/* <Sponsor /> */}
+      <Footer />
+    </div>
+  );
+};
+
+export default PageMerch;
