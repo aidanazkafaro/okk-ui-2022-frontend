1016e64cfae11b62ea3475635ffe74303aa4cd28
aidanazkafaro <aidan.azkafaro@ui.ac.id>
2022-08-01 15:13:32 +0700

    add misi 2

--- src/components/Misi/MisiData.js
+++ src/components/Misi/MisiData.js
@@ -21,21 +21,21 @@ export const misiItems = [
       "\n\nTeknis pengerjaan :\n1.Teman teman agnibrata makara dapat  mengakses link diatas atau dapat mengunjungi website OKK UI 2022\n2. Kalian akan diarahkan menuju Google drive yang berisi contoh tugas, instruksi tugas,  tutorial pengerjaan misi serta twibbon misi.\n3. Teman teman agnibrata makara dapat mengerjakan misi sesuai dengan instruksi, kemudian mengupload reels dan link postingannya diberikan kepada mentor masing masing. \n4. Pastikan instgaram tidak di private serta postinganmu tidak di hapus dari akun instagram hingga akhir rangkaian kegiatan OKK UI 2022\n\nInformasi tambahan :\n✅Pada misi 1 OKK UI 2022 bekerja sama dengan kumparan. Pihak kumparan akan memberikan pelatihan video production 101 pada tanggal 26 Juli 2022 pukul 19.00 WIB melalui live streaming Youtube OKK UI. \n✅Tunjukkan kontribusi terbaikmu karena pada misi 1 kali ini terdapat hadiah dengan total 15 jt rupiah kepada 10 orang pemenang. Cek instagram OKK UI untuk melihat persyaratan pemenang #OKKUIxkumparan\n\n#BersamaBereksplorasi\nOKK UI 2022",
     disable: false,
   },
-  // {
-  //   judul: "Jamuan Cendekia",
-  //   misiKe: "#Misi 2",
-  //   misi: "TTS dan Yel-Yel Kelompok",
-  //   logo: misi2,
-  //   startMonth: "AUGUST-2022",
-  //   range: {
-  //     start: "2022-8-1",
-  //     end: "2022-8-7",
-  //   },
-  //   deskripsi:
-  //     "Selamat, kamu telah memulai jejak awal dalam lembaran baru! Pada misi ini, perjalanan selanjutnya akan dilaksanakan untuk mengenaksal lebih baik mengenai lokasi di kampus dengan berbagai macam teka-teki.\nSelain ada clue yang tersembunyi, kalian juga bertemu dengan teman-teman dari berbagai fakultas. Ayo, lakukan sambil menggemakan yel-yel bersama dengan kelompokmu!",
-  //   linkPengumpulan: "https://okk.ui.ac.id/",
-  //   disable: true,
-  // },
+  {
+    judul: "Jamuan Cendekia",
+    misiKe: "#Misi 2",
+    misi: "TTS dan Yel-Yel Kelompok",
+    logo: misi2,
+    startMonth: "AUGUST-2022",
+    range: {
+      start: "2022-8-1",
+      end: "2022-8-7",
+    },
+    deskripsi:"✨Halo Agnibrata Makara!✨\nGimana Misi 1? Seru kann? Nah setelah mengerjakan Misi 1, Agnibrata Makara akan diajak untuk mengerjakan Misi 2, yaitu Jamuan Cendekia.\nLink penugasan dapat diakses pada:\n\n",
+    link: "bit.ly/Misi2OKKUI2022",
+    deskripsi2: "\n\nTeknis pengerjaan:\n1. Teman-teman Agnibrata Makara dapat mengakses link di atas atau dapat mengunjungi website OKK UI 2022.\n2. Kalian akan diarahkan menuju Google Drive yang berisi folder instruksi tugas dan template TTS.\n3. Teman-teman Agnibrata Makara dpat mengerjakan misi sesuai dengan instruksi pada template TTS yang sudah disediakan. Kemudian jika TTS sudah terisi penuh, TTS dikumpulkan kepada mentor masing-masing kelompok.\n4. Perhatikan Instagram @okk_ui setiap harinya untuk memperoleh petunjuk pengerjaan tugas ya.\n\nSelamat mengeksplorasi kampus UI! 😁\n\n#OKKUI2022\n#BersamaBereksplorasi",
+    disable: false,
+  },
   // {
   //   judul: "Pesta Pikiran",
   //   misiKe: "#Misi 3",
